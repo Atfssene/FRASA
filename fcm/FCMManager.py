7bd33edf2735c05cc0b37687cdb282945993ca0b
Zidni Imani <zidni.imani@gmail.com>
2021-06-06 12:32:51 +0700

    fcm and python

---
+++ fcm/FCMManager.py
@@ -0,0 +1,36 @@
+import firebase_admin
+import datetime
+from firebase_admin import credentials, messaging
+
+
+cred = credentials.Certificate("D:/Bangkit/FRASA/fcm/serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+def sendPush(title, msg, registration_token, dataObject=None):
+    # See documentation on defining a message payload.
+    message = messaging.MulticastMessage(
+        notification=messaging.Notification(
+            title='$GOOG up 1.43% on the day',
+            body='$GOOG gained 11.80 points to close at 835.67, up 1.43% on the day.',
+        ),
+        android=messaging.AndroidConfig(
+            ttl=datetime.timedelta(seconds=3600),
+            priority='normal',
+            notification=messaging.AndroidNotification(
+                icon='stock_ticker_update',
+                color='#ff9163'
+            ),
+        ),
+        apns=messaging.APNSConfig(
+            payload=messaging.APNSPayload(
+                aps=messaging.Aps(badge=42),
+            ),
+        ),
+        data=dataObject,
+        tokens=registration_token,
+    )
+
+    # Send a message to the device corresponding to the provided
+    # registration token.
+    response = messaging.send_multicast(message)
+    # Response is a message ID string.
+    print('Successfully sent message:', response)
---
+++ fcm/SendPush.py
@@ -0,0 +1,5 @@
+import FCMManager as fcm
+
+tokens = ["fmCAvLKtS8CTX6lNIWntI-:APA91bGStZMtZDQZlSDhxl1LlJn6SXxx29dEEZWdiXn6fWHtG1"
+            "ev_Qf6ge4dTOLoDSiQ08zO4n3iYw6bOllsSaBzr2m2Ml5lW-TXJEahy493YtB0J_5svdOj2GTIU_ZRBfQeixM_Mmfy"]
+fcm.sendPush("Not FRASA yet", "This is push notification for FRASA later.", tokens)
