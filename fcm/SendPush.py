90b6b19aa2ba97743e3172248f6c54ed19991ad1
Zidni Imani <zidni.imani@gmail.com>
2021-06-07 17:25:58 +0700

    delete unused file

--- fcm/SendPush.py
+++
@@ -1,4 +0,0 @@
-import FCMManager as fcm
-
-tokens = ["eqEAZB1SQyOTniLWc0yPuT:APA91bFQa5BLcvqVVgh51bs79PaeHJEpJGExSzisSc5gadUHEuluqexxtoBZC0xlNPWj1r5us8_FUKjJZ3GCdFPPAVl1yirJggIF8SfUlahqvP5woO_8YKlBg1zUFjxJF0ruO9BUnvR6"]
-fcm.sendPush("Konten FRASA langgananmu menunggu!", "Notifikasi ini dibuat dengan Python", tokens)
